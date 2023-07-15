e15c659d6d658747b7140edacf30f6cf846537fe
Galact07 <sidharthnair1907@gmail.com>
2023-07-15 15:02:05 +0530

    Forgot Password Added

--- src/helpers/mail.ts
+++ src/helpers/mail.ts
@@ -36,9 +36,7 @@ export const SendEmail = async({userId,email,emailTYPE}:any)=>{
             subject: emailTYPE == "VERIFY" ? "Verify EMAIL" : "Reset Password email", // Subject line
             text: "Hello USER", // plain text body
             html: `<p>Click on the url to <a href="${process.env.DOMAIN}/${emailTYPE == "VERIFY" ?`verifyMail?token=${token}`:`forgotPassword?token=${token}`}">Link</a> to 
-            ${emailTYPE == "VERIFY" ? "verify your email " : "reset password "}</p> <br/>
-            "${process.env.DOMAIN}/verifyMail?token=${token}"</br>
-            "${process.env.DOMAIN}/forgotPassword?token=${token}"`, 
+            ${emailTYPE == "VERIFY" ? "verify your email " : "reset password "}</p> <br/>`, 
           });
 
           return sender;
