2148171e78c7c13648a45deb8df8c3d9c0ad4939
Galact07 <sidharthnair1907@gmail.com>
2023-07-15 15:09:38 +0530

    Forgot Password Added

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        TOKEN_SECRET:"sidosonrolltoday",
-DOMAIN:"http://localhost:3000",
-MAILTRAP_USER:"610c4b10ab255e",
-MAILTRAP_PASSWORD:"1a59fa6f96a30a",
-MONGO_URL:"mongodb+srv://sidnair07:sirinair09@cluster0.zzplxyx.mongodb.net/"
+        
     }
 }
 
